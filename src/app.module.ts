903a9080e21d064f6f0ec4c355d7a8973717892d
boykovm <66530211+boykovm@users.noreply.github.com>
2025-12-14 23:59:31 +0200

    updated configuration service (#3)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
+      load: [configuration],
     }),
   ],
   controllers: [AppController],
